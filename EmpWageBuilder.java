4a485841bf3b91873bbfb99c997fcf09b2de6fd0
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-31 11:37:52 +0530

    Added ability to compute Emp Wage for multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,10 +2,7 @@ public class EmpWageBuilder
 {
   public static final int IS_PART_TIME = 1;
  public static final int IS_FULL_TIME = 2;
- public static final int EMP_RATE_PER_HOUR = 20;
-public static final int NUM_OF_WORKING_DAYS = 2;
-public static final int MAX_HRS_IN_MONTH = 10;
- public static int computeEmpWage()
+ public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
  {
   int empHrs = 0;
   int totalEmpHrs = 0;
@@ -34,7 +31,8 @@ int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 }
 public static void main(String[] args)
 {
-  computeEmpWage( );
+  computeEmpWage("DMart", 20, 2, 10);
+  computeEmpWage("Reliance", 10, 4, 20);
 
 }
 }
