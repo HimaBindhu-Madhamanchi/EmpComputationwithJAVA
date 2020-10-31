d8a7bfefb3c665ee84a15fcc5f1637407ac0898a
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-31 10:45:26 +0530

    Refactor the code to write a class method to compute Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,7 +5,7 @@ public class EmpWageBuilder
  public static final int EMP_RATE_PER_HOUR = 20;
 public static final int NUM_OF_WORKING_DAYS = 2;
 public static final int MAX_HRS_IN_MONTH = 10;
- public static void main(String[] args)
+ public static int computeEmpWage()
  {
   int empHrs = 0;
   int totalEmpHrs = 0;
@@ -30,5 +30,11 @@ totalEmpHrs += empHrs;
 }
 int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
   System.out.println("Total Emp Wage: " + totalEmpWage);
-  }
+ return totalEmpWage;  
+}
+public static void main(String[] args)
+{
+  computeEmpWage( );
+
+}
 }
